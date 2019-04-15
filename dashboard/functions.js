b8c6639b308de0d0f95773596fad414685f12d53
xCall <carlosfaita99@gmail.com>
2019-04-14 23:46:54 -0300

    Menu Scrolling
    menu scrolling com com cor e animacao, falta arrumar sidebar

---
+++ dashboard/functions.js
@@ -0,0 +1,33 @@
+function mostrarAtivo(tag){
+    var tag_li = document.getElementById('lista-menu');
+    var tag_a = tag_li.getElementsByTagName('a');
+    for (i=0; i<tag_a.length; i++ ){
+        tag_a[i].style.color = "";
+        tag_a[i].style.background = "";
+        tag_a[i].style.boxShadow = "";
+        tag_a[i].style.borderLeft = "";
+    }
+    tag.style.color = "#777";
+    tag.style.background = "#fff";
+    tag.style.boxShadow = "0 2px 3px #777";
+    tag.style.borderLeft = "9px solid red";
+}
+function abrirSlideMenu(){
+    var menu = document.getElementById('menu');
+    var content = document.getElementById('content');
+    content.style.marginLeft = "0";
+    menu.style.width = "200px";
+    if(scrollTop > 50){
+        menu.style.marginTop = "60px";
+        content.style.marginTop = "60px";
+    }
+}
+function fecharSlideMenu(){
+    var menu = document.getElementById('menu');
+    var content = document.getElementById('content');
+    
+    menu.style.widht = "-200px";
+    content.style.marginLeft = "-200px";
+    content.style.left = "-200px";
+    menu.style.transition = "0.7s";
+}
