552c84767f4bc9b88c118c8da193bf2643bc5fd9
xCall <carlosfaita99@gmail.com>
2019-05-02 23:13:44 -0300

    VERSAO DESKTOP FINALIZADA
    falta finalizar a versao mobile

--- dashboard/functions.js
+++ dashboard/functions.js
@@ -1,3 +1,14 @@
+/*
+    Versao: 0.1.0
+    Ultima atualizacao: 02/05/2019
+    Criado por: carlosfaita99@gmail.com
+    modificado em:                  por:
+    modificacoes realizadas: 
+    ***NAO ESQUECA DE FAZER COMENTARIOS***
+*/
+
+//Funcao para adicionar e remover classes
+//de acordo coom o scrool da pagina usando o JQUERY
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('nav').addClass('scrollNavbar');
@@ -7,6 +18,8 @@ $(window).scroll(function() {
         $('main').removeClass('scrollMenuLateral');
     }
 });
+//Funcao para mostrar no NavBar em qual 
+//local da pagina a pessoa esta localizada
 function mostrarAtivo(tag){
     var tag_li = document.getElementById('lista-menu');
     var tag_a = tag_li.getElementsByTagName('a');
@@ -21,17 +34,17 @@ function mostrarAtivo(tag){
     tag.style.boxShadow = "0 2px 3px #777";
     tag.style.borderLeft = "9px solid red";
 }
+//Funcao usada para abrir a Navbar
 function abrirSlideMenu(){
     var menu = document.getElementById('menu');
     var content = document.getElementById('content');
     content.style.marginLeft = "0";
     menu.style.width = "200px";
-
 }
+//Funcao usada para fechar a Navbar
 function fecharSlideMenu(){
     var menu = document.getElementById('menu');
     var content = document.getElementById('content');
-    
     menu.style.widht = "-200px";
     content.style.marginLeft = "-200px";
     content.style.left = "-200px";
